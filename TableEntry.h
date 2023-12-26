ec41eff01faff1e535a42fabce253fde565346ea
Vicent Martinez de la Concepción <vmardel@epsa.edu.es>
2023-12-26 13:26:50 +0100

    finalizacion del archivo TableEntry.h y su comprobacion para que salga lo esperado por pantalla, junto con el Makefile

--- TableEntry.h
+++ TableEntry.h
@@ -6,11 +6,26 @@
 
 template <typename V> 
 class TableEntry {
-    public:
-	std::string key
-	V value
-
-    
+public:
+    std::string key;
+    V value;
+    TableEntry(std::string key, V value) : key(key), value(value) {}
+    TableEntry(std::string key) : key(key), value() {}
+    TableEntry() : key(""), value() {}
+    // Sobrecarga del operador == 
+    friend bool operator==(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key == te2.key;
+    }
+    // Sobrecarga del operador != 
+    friend bool operator!=(const TableEntry<V> &te1, const TableEntry<V> &te2) {
+        return te1.key != te2.key;
+    }
+    // Sobrecarga del operador <<
+    friend std::ostream& operator<<(std::ostream &out, const TableEntry<V> &te) {
+        out << "(" << te.key << ", " << te.value << ")";
+        return out;
+    }
 };
 
 #endif
+
