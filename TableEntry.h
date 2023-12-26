ba023021c6ce015dce3e2f7833a42cfd1bba6b13
Vicent Martinez de la Concepción <vmardel@epsa.edu.es>
2023-12-26 12:29:24 +0100

    prueba de verificación tokken

---
+++ TableEntry.h
@@ -0,0 +1,16 @@
+#ifndef TABLEENTRY_H
+#define TABLEENTRY_H
+
+#include <string>
+#include <ostream>
+
+template <typename V> 
+class TableEntry {
+    public:
+	std::string key
+	V value
+
+    
+};
+
+#endif
