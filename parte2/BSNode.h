df8f6beb8d9216f5dd63ad57b810f5d48183493f
Vicent Martinez de la Concepción <vmardel@epsa.edu.es>
2023-12-27 18:28:10 +0100

    archivo BSNode.h

---
+++ parte2/BSNode.h
@@ -0,0 +1,26 @@
+#ifndef BSNODE_H
+#define BSNODE_H
+
+#include <ostream>
+
+template <typename T>
+class BSNode {
+public:
+    // Atributos
+    T elem;            // Elemento de tipo T almacenado en el nodo.
+    BSNode<T>* left;   // Puntero al nodo sucesor izquierdo.
+    BSNode<T>* right;  // Puntero al nodo sucesor derecho.
+
+    // Métodos
+    // Constructor
+    BSNode(T elem, BSNode<T>* left = nullptr, BSNode<T>* right = nullptr) : elem(elem), left(left), right(right) {}
+
+    // Sobrecarga del operador <<
+    friend std::ostream& operator<<(std::ostream& out, const BSNode<T>& bsn) {
+        out << bsn.elem;
+        return out;
+    }
+};
+
+#endif
+
