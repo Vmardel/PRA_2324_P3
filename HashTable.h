f164f7e136d7b3904938a777331724aed8d62c24
Vicent Martinez de la Concepción <vmardel@epsa.edu.es>
2023-12-28 20:04:11 +0100

    archivos primera parte practica 3

--- HashTable.h
+++ HashTable.h
@@ -1,62 +1,98 @@
-#ifndef HASH_TABLE_H
-#define HASH_TABLE_H
+#ifndef HASHTABLE_H
+#define HASHTABLE_H
 
 #include <ostream>
 #include <stdexcept>
-#include <string>
 #include "Dict.h"
 #include "TableEntry.h"
+
 #include "../PRA_2324_P1/ListLinked.h"
 
 template <typename V>
-class HashTable : public Dict<V> {
-private:
-    int n;
-    int max;
-    ListLinked<TableEntry<V>>* table;
+class HashTable: public Dict<V> {
 
-    int h(std::string key) {
-        int sum = 0;
-        for (char c : key) {
-            sum += static_cast<int>(c);
-        }
-        return sum % max;
-    }
+	    private:
+		    int n;
+		    int max;
+		    ListLinked<TableEntry<V>>* table;
+		    int h(std::string key){
+                            int acu=0;
+                            for(char c : key){
+                                acu += c;
+                            }
+                            return acu % max;
+                    }
+       
+	    public:
+		    HashTable(int size){
+			n = 0;
+		    	max = size;
+			table = new ListLinked<TableEntry<V>>[size]();
+		    }
+		    ~HashTable(){
+		    	delete[] table;
+		    }
 
-public:
-    HashTable(int size) : n(0), max(size) {
-        table = new ListLinked<TableEntry<V>>[max];
-    }
+		    int capacity(){
+			return max;
+		    }
+		    
+		    friend std::ostream& operator<<(std::ostream &out, const HashTable<V> &th){
+		    	out << "HashTable [entries: "<< th.n <<", capacity: " << th.max << "]"<< std::endl;
+			out << "==============" << std::endl << std::endl;
+			for(int i=0; i<th.max; i++){
+				out << " == Cubeta " << i <<" ==" <<std::endl << std::endl;
+				out << "List => ";
+				if(th.table[i].size() != 0){
+					out << std::endl;
+					for(int j = 0; j < th.table[i].size(); j++){
+						out << "  ('" << th.table[i].get(j).key << "' => " << th.table[i].get(j).value << ")"<< std::endl;
+					}
+				}
+			}
+			out << "==============" << std::endl;
+			return out;
+		    }
 
-    ~HashTable() {
-        delete[] table;
-    }
+		    V operator[](std::string key){
+			return search(key);
+		    }
 
-    int capacity() const {
-        return max;
-    }
+		    void insert(std::string key, V value){
+			try{	
+		    	    	search(key);	
 
-    friend std::ostream& operator<<(std::ostream& out, const HashTable<V>& th) {
-        for (int i = 0; i < th.max; ++i) {
-            out << "Cubeta " << i << ": ";
-            for (auto it = th.table[i].begin(); it != th.table[i].end(); ++it) {
-                out << "[" << it->key << ", " << it->value << "] ";
-            }
-            out << std::endl;
-        }
-        return out;
-    }
+			}catch(std::runtime_error& e){
+				table[h(key)].prepend(TableEntry<V>(key, value));
+				n++;
+		    		return;
 
-    V operator[](std::string key) {
-        int index = h(key);
-        for (auto it = table[index].begin(); it != table[index].end(); ++it) {
-            if (it->key == key) {
-                return it->value;
-            }
-        }
-        throw std::runtime_error("Clave no encontrada");
-    }
+			}
+			throw std::runtime_error("key '"+key+"' already exists!");
+		    }
+
+		    V search(std::string key){
+		    	for(int i = 0;  i<table[h(key)].size(); i++){
+				if(table[h(key)].get(i).key == key)
+					return table[h(key)].get(i).value;
+			}
+			throw std::runtime_error("Key '"+key+"' not found!");	
+		    }
+		    V remove(std::string key){
+			    for(int i = 0;  i< table[h(key)].size() ;i++){
+				    if(table[h(key)].get(i).key == key){				    
+					    TableEntry<V> val=table[h(key)].remove(i);
+					    n--;
+					    return val.value;
+				    }
+			    }
+			    throw std::runtime_error("Key '"+key+"' not found!");
+		    }
+		    
+		    int entries(){
+			    return n;
+		    }
+
 };
 
 #endif
-
